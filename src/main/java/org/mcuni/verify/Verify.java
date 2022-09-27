73f83809220d0132de8bd1d6f971d39f6f6f795a
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-09-27 16:21:52 +0100

    Ported from Bukkit

--- src/main/java/org/mcuni/verify/Verify.java
+++ src/main/java/org/mcuni/verify/Verify.java
@@ -6,9 +6,14 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
+import net.kyori.adventure.text.Component;
 import org.slf4j.Logger;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Scanner;
 
 @Plugin(
         id = "verify",
@@ -17,6 +22,10 @@ import javax.annotation.Nullable;
 )
 public class Verify {
 
+    public String ServerNickname = "UOPMC";
+    public String UniversityName = "the University of Plymouth";
+    public String NetworkID = "PLYMOUTH";
+
     @Inject
     private Logger logger;
 
@@ -24,8 +33,47 @@ public class Verify {
     public void onProxyInitialization(ProxyInitializeEvent event) {
     }
 
+    public Component kickMessage() {
+        return Component.text("Welcome to "+ServerNickname +
+                "\n" +
+                "This server is only for students at "+UniversityName+".\n" +
+                "You need to verify that you're a student before you can join.\n" +
+                "\n" +
+                "To verify your account please visit mcuni.org/verify");
+    }
+
     @Subscribe
     public void ServerPostConnectEvent(Player player, @Nullable RegisteredServer previousServer) {
         logger.debug(player.getUsername()+" connected.");
+        if (!getUserInfo(player.getUsername(), String.valueOf(player.getUniqueId()))) {
+            logger.debug("[Kit][Whitelist] Kicked player '"+player.getUsername()+"'. Player is not registered.");
+            player.disconnect(kickMessage());
+        } else {
+            logger.debug("[Kit][Whitelist] Allowed player '"+player.getUsername()+"'. Player is registered.");
+        }
+    }
+
+    private boolean getUserInfo(String username, String uuid) {
+        try {
+            logger.debug("[Kit][Whitelist] Fetching player data for user '"+username+"' with UUID '"+uuid+"'.");
+            URL url = new URL("https://kit.mcuni.org/api/v1/user.php?username="+username+"&uuid="+uuid+"&network="+NetworkID);
+            logger.debug("[DEBUG] https://kit.mcuni.org/api/v1/user.php?username="+username+"&uuid="+uuid+"&network="+NetworkID);
+            Scanner s = new Scanner(url.openStream());
+            if (s.hasNextLine()) {
+                String response = s.nextLine();
+                if (response.equals("")) {
+                    logger.debug("[Kit][Whitelist] There was no response from the server.");
+                    return false;
+                } else {
+                    logger.debug("[Kit][Whitelist] Fetched: " + response);
+                    return response.equals("true");
+                }
+            }
+        }
+        catch(IOException ex) {
+            logger.debug("[Kit][Broadcast] Fatal error.");
+            logger.debug(Arrays.toString(ex.getStackTrace()));
+        }
+        return false;
     }
 }
